b282c8a0157a871280f00e2d7f7c1d4c45eb5b60
Yosef <omniarchelite@gmail.com>
2022-05-04 19:18:36 +0300

    removed manage stock page

--- Qemer_POS/routes/web.php
+++ Qemer_POS/routes/web.php
@@ -56,5 +56,4 @@ Route::controller(ReceiptController::class)->group( function (){
 
 Route::controller(CategoryController::class)->group( function (){
     Route::post('addCategories', 'store');
-    Route::get('/itemForm','displayForm');
 });
