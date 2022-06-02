fc91187b1d8de952a3ba96abc8c93ac934c8b7ac
Yonas <yonaslegesse19@gmail.com>
2022-06-02 23:45:19 +0300

    modified cards

--- Qemer_POS/resources/views/Templates/company-report.blade.php
+++ Qemer_POS/resources/views/Templates/company-report.blade.php
@@ -1,14 +1,14 @@
 <div class="home_content" style="float:right;">
 
-    <div class="row justify-content-evenly">
+    <div class="row justify-content-evenly align-items-center ">
         <div class="col-md-3">
             <div class="container shadow" style=" background-color:	#fffefece; margin-top:3rem;height:110px;border-radius:20px">
                 <!-- //row for Contents -->
-                <div class="row">
+                <div class="row  ">
                     <div class="col-md-3 ">
-                        <div class="container-fluid   my-2 rounded-circle" style="background-color:yellow;width:70px;height:70px">
+                        <div class="container-fluid  px-3 py-0  my-4 rounded-circle" style="background-color:rgb(209, 84, 56);width:70px;height:70px">
                             <!-- //icon for users -->
-                            <i class=" px-0 bi bi-people fs-1 text-white text-center"></i>
+                            <i class="  bi bi-people fs-1 text-white"></i>
                         </div>
                     </div>
                     <div class="col my-auto">
@@ -16,7 +16,7 @@
                             <p class="text-dark my-auto  fs-5">Total users</p>
                         </div>
                         <div class="col-md-8 my-0 ">
-                            <p class="text-muted fs-6">{!! $users !!}</p>
+                            <p class="text-danger fs-6">{!! $users !!}</p>
                         </div>
                     </div>
 
@@ -29,9 +29,9 @@
                 <!-- //row for Contents -->
                 <div class="row">
                     <div class="col-md-3 ">
-                        <div class="container-fluid   my-2 rounded-circle" style="background-color:purple;width:70px;height:70px">
+                        <div class="container-fluid  px-3 py-0  my-4 rounded-circle" style="background-color:purple;width:70px;height:70px">
                             <!-- //icon for users -->
-                            <i class=" px-0 bi bi-cart fs-1 text-white text-center"></i>
+                            <i class="  bi bi-cart fs-1 text-white"></i>
                         </div>
                     </div>
                     <div class="col my-auto">
@@ -53,9 +53,9 @@
                 <!-- //row for Contents -->
                 <div class="row">
                     <div class="col-md-3 ">
-                        <div class="container-fluid   my-2 rounded-circle" style="background-color:green;width:70px;height:70px">
+                        <div class="container-fluid  px-3 py-0  my-4 rounded-circle" style="background-color:green;width:70px;height:70px">
                             <!-- //icon for users -->
-                            <i class=" px-0 bi bi-currency-dollar fs-1 text-white text-center"></i>
+                            <i class="  bi bi-currency-dollar fs-1 text-white text-center"></i>
                         </div>
                     </div>
                     <div class="col  "style="margin-top:10px">
@@ -63,8 +63,8 @@
                             <p class="text-dark my-auto py-auto fs-5">Sales</p>
                         </div>
                         <div class="col my-0 py-0">
-                            <p class="text-warning fs-6">36870.32 Br</p>
-                            <small class="text-muted">{{ now()->format('Y-m-d') }}</small>
+                            <p class="text-success fs-6">36870.32 Br</p>
+                            <small class="text-muted "> Date: {{ now()->format('Y/m/d') }}</small>
 
                         </div>
                     </div>
